01ae56afe03bc7061a49dc8f177f58a22cc26752
chrisss101 <Crafaniello9535@bths.edu>
2018-12-13 15:44:09 -0500

    Initial commit

---
+++ src/BarChar.java
@@ -0,0 +1,9 @@
+public class BarChar {
+    final static String austria = "Austria";
+    final static String brazil = "Brazil";
+    final static String france = "France";
+    final static String italy = "Italy";
+    final static String usa = "USA";
+
+    
+}
---
+++ src/CSV.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSV {
+
+
+    public class CSVutilities {
+        ArrayList<String> CSVData;
+        int numColumns;
+
+        public CSVutilities() {
+
+        }
+        public List<String> getColumnHeaders() {
+
+        }
+        public List<String> getDataString(int column) {
+
+        }
+        public List<Integer>getDataInt(int column) {
+
+        }
+    }
+
+}
