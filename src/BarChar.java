23c94ceb328aece3ef616c2e3c7f5b606e7142c6
chrisss101 <Crafaniello9535@bths.edu>
2018-12-14 16:38:21 -0500

    wdawdd

--- src/BarChar.java
+++ src/BarChar.java
@@ -7,11 +7,11 @@ import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 public class BarChar extends Application {
-    final static String austria = "Austria";
-    final static String brazil = "Brazil";
-    final static String france = "France";
-    final static String italy = "Italy";
-    final static String usa = "USA";
+    final static String austria = "BTHS";
+    final static String brazil = "Stuyvesant";
+    final static String france = "Staten Island Tech";
+    final static String italy = "Queens Science";
+    final static String usa = "Bronx Science";
 
     @Override public void start(Stage stage) {
         stage.setTitle("Bar Chart Sample");
@@ -19,33 +19,33 @@ public class BarChar extends Application {
         final NumberAxis yAxis = new NumberAxis();
         final BarChart<String,Number> bc =
                 new BarChart<String,Number>(xAxis,yAxis);
-        bc.setTitle("Country Summary");
-        xAxis.setLabel("Country");
-        yAxis.setLabel("Value");
+        bc.setTitle("School SAT");
+        xAxis.setLabel("School");
+        yAxis.setLabel("Score");
 
         XYChart.Series series1 = new XYChart.Series();
         series1.setName("2003");
-        series1.getData().add(new XYChart.Data(austria, 25601.34));
-        series1.getData().add(new XYChart.Data(brazil, 20148.82));
-        series1.getData().add(new XYChart.Data(france, 10000));
-        series1.getData().add(new XYChart.Data(italy, 35407.15));
-        series1.getData().add(new XYChart.Data(usa, 12000));
+        series1.getData().add(new XYChart.Data(austria, 659));
+        series1.getData().add(new XYChart.Data(brazil, 735));
+        series1.getData().add(new XYChart.Data(france, 682));
+        series1.getData().add(new XYChart.Data(italy, 660));
+        series1.getData().add(new XYChart.Data(usa, 688));
 
         XYChart.Series series2 = new XYChart.Series();
         series2.setName("2004");
-        series2.getData().add(new XYChart.Data(austria, 57401.85));
-        series2.getData().add(new XYChart.Data(brazil, 41941.19));
-        series2.getData().add(new XYChart.Data(france, 45263.37));
-        series2.getData().add(new XYChart.Data(italy, 117320.16));
-        series2.getData().add(new XYChart.Data(usa, 14845.27));
+        series2.getData().add(new XYChart.Data(austria, 659));
+        series2.getData().add(new XYChart.Data(brazil, 735));
+        series2.getData().add(new XYChart.Data(france, 682));
+        series2.getData().add(new XYChart.Data(italy, 660));
+        series2.getData().add(new XYChart.Data(usa, 688));
 
         XYChart.Series series3 = new XYChart.Series();
         series3.setName("2005");
-        series3.getData().add(new XYChart.Data(austria, 45000.65));
-        series3.getData().add(new XYChart.Data(brazil, 44835.76));
-        series3.getData().add(new XYChart.Data(france, 18722.18));
-        series3.getData().add(new XYChart.Data(italy, 17557.31));
-        series3.getData().add(new XYChart.Data(usa, 92633.68));
+        series3.getData().add(new XYChart.Data(austria, 659));
+        series3.getData().add(new XYChart.Data(brazil, 735));
+        series3.getData().add(new XYChart.Data(france, 682));
+        series3.getData().add(new XYChart.Data(italy, 660));
+        series3.getData().add(new XYChart.Data(usa, 688));
 
         Scene scene  = new Scene(bc,800,600);
         bc.getData().addAll(series1, series2, series3);
